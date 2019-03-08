b5453bc006711b14a8d06721929bcfd0ca4a251f
Luke Childs <lukechilds123@gmail.com>
2019-03-08 14:03:24 +0700

    Test getKomodoRewards() returns a number

--- test/unit.js
+++ test/unit.js
@@ -4,3 +4,7 @@ import getKomodoRewards from '..';
 test('getKomodoRewards is exported', t => {
 	t.not(getKomodoRewards, undefined);
 });
+
+test('getKomodoRewards() returns a number', t => {
+	t.is(0, getKomodoRewards({tiptime: 1, locktime: 1, height: 1, satoshis: 1}));
+});
