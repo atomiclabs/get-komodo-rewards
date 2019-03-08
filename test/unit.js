7eda79f449594d41a28404b55cfefeffbc8a20cd
Luke Childs <lukechilds123@gmail.com>
2019-03-08 14:04:24 +0700

    Test getKomodoRewards() throws a TypeError if tiptime property is not a number

--- test/unit.js
+++ test/unit.js
@@ -8,3 +8,11 @@ test('getKomodoRewards is exported', t => {
 test('getKomodoRewards() returns a number', t => {
 	t.is(0, getKomodoRewards({tiptime: 1, locktime: 1, height: 1, satoshis: 1}));
 });
+
+test('getKomodoRewards() throws a TypeError if tiptime property is not a number', t => {
+	const error = t.throws(() => {
+		getKomodoRewards({tiptime: undefined, locktime: 1, height: 1, satoshis: 1});
+	});
+	t.true(error instanceof TypeError);
+	t.is(error.message, '`tiptime` option must be a number.');
+});
