858f30e788a696b210b1d931db25833eac999342
Luke Childs <lukechilds123@gmail.com>
2019-03-13 13:25:45 +0700

    Test getKomodoRewards() correctly calculates rewards

---
+++ test/unit.js
@@ -0,0 +1,26 @@
+import test from 'ava';
+import {subMinutes, subYears} from 'date-fns';
+import getKomodoRewards from '..';
+
+const KOMODO_ENDOFERA = 7777777;
+const LOCKTIME_THRESHOLD = 500000000;
+const MIN_SATOSHIS = 1000000000;
+const ONE_MONTH_CAP_HARDFORK = 1000000;
+const ONE_HOUR = 60;
+const ONE_MILLISECOND = 1000;
+
+const tiptime = 1552455573;
+const locktime = Math.floor(subMinutes(tiptime * ONE_MILLISECOND, ONE_HOUR).getTime() / ONE_MILLISECOND);
+const height = ONE_MONTH_CAP_HARDFORK;
+const satoshis = MIN_SATOSHIS;
+
+test('getKomodoRewards() correctly calculates rewards', t => {
+	const utxo = {
+		tiptime,
+		locktime,
+		height,
+		satoshis
+	};
+
+	t.is(95, getKomodoRewards(utxo));
+});
